2811a02133f23254594e706e6bbd4e3a4eb041e2
cackharot <cackharot@gmail.com>
2016-03-20 10:37:35 +0530

    added hlint, doctest, haddock to build process

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+{- |
+    An someFunc prints "someFunc" on the console when run
+
+    >>> someFunc
+    someFunc
+-}
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
